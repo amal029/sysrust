089a87fc8dea8ea74c624c61f324cf596b50cb3c
Avinash Malik <avinash.malik@auckland.ac.nz>
2024-05-17 11:33:15 +1200

    Updated the extension of the sysrust files as sysrs.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
+pub mod ast;
 mod lexer;
 pub mod tokens;
-pub mod ast;
 // use std::error::Error;
 
 use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(pub og);
 use og::ScriptParser;
 
-pub fn parse(s: &str) {
+pub fn parse(s: &str) -> Vec<ast::Stmt> {
     let source_code = std::fs::read_to_string(s).unwrap();
     // println!("source code: {} {}", source_code, source_code.len());
     let lexer = lexer::Lexer::new(&source_code);
     let parser = ScriptParser::new();
     let ast = parser.parse(lexer).unwrap();
-    println!("{:?}", ast);
+    return ast;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use sysrust::parse;
 use std::env;
-use std::io;
+use sysrust::parse;
 
-fn main() -> io::Result<()>{
+fn main() {
     let args: Vec<String> = env::args().collect();
-    // let mut f = File::open("first.txt")?;
-    // let mut source_code = String::new();
-    // f.read_to_string(&mut source_code)?;
-    // let source_code = std::fs::read_to_string(&args[1]).unwrap();
-    // println!("source code: {} {}", source_code, source_code.len());
-    let _ = parse(&args[1]);
-    Ok(())
+    let _ast = parse(&args[1]);
+    println!("{:?}", _ast);
 }
