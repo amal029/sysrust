865bf9db19a198c79b57502d3ff48ab2d2e6e17f
Avinash Malik <avinash.malik@auckland.ac.nz>
2024-05-17 19:05:18 +1200

    Updated a comment about abort and suspend transformations.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod rewrite;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let _ast = parse(&args[1]);
-    // XXX: Now rewrite the immediate abort and suspend statement
     let mut _state = rewrite::State::new();
+    // XXX: Now rewrite the immediate abort and suspend statement
     let _ast = rewrite::rewrite_stmts(_ast, &mut _state);
     println!("{:?}", _ast);
 }
