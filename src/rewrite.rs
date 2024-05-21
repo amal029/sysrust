09cf54ba3fb7cab8d98c12696824ae75668e629a
Avinash Malik <avinash.malik@auckland.ac.nz>
2024-05-21 18:56:06 +1200

    Started writing the FSM/Graph rewrite for sysrust.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- src/rewrite.rs
+++ src/rewrite.rs
@@ -41,7 +41,7 @@ fn imm_rewrite(_ast: Stmt, state: &mut State) -> Stmt {
                     p,
                 ),
                 ASQual::Weak => Stmt::Abort(sy, Some(ASQual::Weak), Box::new(s), p),
-                ASQual::WeakImmediate => panic!("Weak and immediate abort !supported"),
+                ASQual::WeakImmediate => todo!("Weak and immediate abort"),
             },
             None => Stmt::Abort(sy, a, Box::new(s), p),
         }
@@ -65,7 +65,7 @@ fn imm_rewrite(_ast: Stmt, state: &mut State) -> Stmt {
                     Stmt::Block(vec![ab, sus], p)
                 }
                 ASQual::Weak => Stmt::Abort(sy, Some(ASQual::Weak), Box::new(s), p),
-                ASQual::WeakImmediate => panic!("Weak and immediate suspend !supported"),
+                ASQual::WeakImmediate => todo!("Weak and immediate suspend"),
             },
             None => Stmt::Abort(sy, a, Box::new(s), p),
         }
@@ -98,28 +98,16 @@ fn imm_rewrite(_ast: Stmt, state: &mut State) -> Stmt {
 
 // XXX: Write the function to rewrite statements to the fsm graph
 
-#[allow(dead_code)]
 #[derive(Debug, Clone)]
 struct GraphNode {
-    pub children: Vec<usize>, // this is the destination state
-    pub parents: Vec<usize>,  //this is the parent state
-    pub actions: Vec<Stmt>,   // these are the actions on the transitions
-    pub guards: Vec<Expr>,    // these are the guards on transitions
-    pub tag: bool,            // this is to tell if this is a real state of
-                              // dummy state
-}
-
-#[allow(dead_code)]
-impl GraphNode {
-    fn new(child: usize, parent: usize, action: Stmt, guard: Expr, t: bool) -> Self {
-        Self {
-            children: vec![child],
-            parents: vec![parent],
-            actions: vec![action],
-            guards: vec![guard],
-            tag: t,
-        }
-    }
+    children: Vec<usize>, // this is the destination state
+    parents: Vec<usize>,  //this is the parent state
+    actions: Vec<Stmt>,   // these are the actions on the transitions
+    guards: Vec<Expr>,    // these are the guards on transitions
+    tag: bool,            // this is to tell if this is a real state of
+    // dummy state
+    label: String,
+    idx: Index,
 }
 
 impl Default for GraphNode {
@@ -130,12 +118,222 @@ impl Default for GraphNode {
             actions: vec![],
             guards: vec![],
             tag: false,
+            label: String::from("Initial"),
+            idx: 0,
         }
     }
 }
 
+type Index = usize;
+
+fn rewrite_stmt_to_graph_fsm(
+    _nodes: &mut Vec<GraphNode>,
+    idx: &mut usize,
+    s: &Stmt,
+) -> (Vec<Index>, Vec<Index>) {
+    fn symbol_string(s: &Symbol) -> String {
+        match s {
+            Symbol::Symbol(ss, _) => ss.clone(),
+        }
+    }
+    match s {
+        Stmt::Emit(a, expr, pos) => {
+            let mut i = GraphNode::default();
+            i.label = String::from("EmitStart");
+            i.actions
+                .push(Stmt::Emit(a.clone(), expr.clone(), pos.clone()));
+            i.guards.push(Expr::True(pos.clone()));
+            let mut e = GraphNode::default();
+            e.label = String::from("EmitEnd");
+            // XXX: Put these in the _nodes
+            _nodes.push(i);
+            _nodes[*idx].children.push(*idx + 1);
+            _nodes[*idx].idx = *idx;
+            let r1 = vec![*idx];
+            *idx += 1;
+            _nodes.push(e);
+            _nodes[*idx].parents.push(*idx - 1);
+            _nodes[*idx].idx = *idx;
+            let r2 = vec![*idx];
+            *idx += 1;
+            (r1, r2)
+        }
+        Stmt::Pause(la, pos) => {
+            let mut s0 = GraphNode::default();
+            s0.label = String::from(symbol_string(&la));
+            s0.tag = true;
+            s0.guards.push(Expr::True(pos.clone()));
+            let mut e = GraphNode::default();
+            e.label = String::from("PauseEnd");
+            // XXX: Add the doubly linked list annotations
+            _nodes.push(s0);
+            _nodes[*idx].children.push(*idx + 1);
+            _nodes[*idx].idx = *idx;
+            let r1 = vec![*idx];
+            *idx += 1;
+            _nodes.push(e);
+            _nodes[*idx].parents.push(*idx - 1);
+            _nodes[*idx].idx = *idx;
+            let r2 = vec![*idx];
+            *idx += 1;
+            (r1, r2)
+        }
+        Stmt::Assign(a, expr, pos) => {
+            let mut i = GraphNode::default();
+            i.actions
+                .push(Stmt::Assign(a.clone(), expr.clone(), pos.clone()));
+            i.guards.push(Expr::True(pos.clone()));
+            let mut e = GraphNode::default();
+            e.label = String::from("End");
+            // XXX: Add the doubly linked list annotations
+            _nodes.push(i);
+            _nodes[*idx].children.push(*idx + 1);
+            _nodes[*idx].idx = *idx;
+            let r1 = vec![*idx];
+            *idx += 1;
+            _nodes.push(e);
+            _nodes[*idx].parents.push(*idx - 1);
+            _nodes[*idx].idx = *idx;
+            let r2 = vec![*idx];
+            *idx += 1;
+            (r1, r2)
+        }
+        Stmt::Signal(a, io, pos) => {
+            let mut i = GraphNode::default();
+            i.label = String::from("SignalStart");
+            i.actions
+                .push(Stmt::Signal(a.clone(), io.clone(), pos.clone()));
+            i.guards.push(Expr::True(pos.clone()));
+            let mut e = GraphNode::default();
+            e.label = String::from("SignalEnd");
+            // XXX: Add the doubly linked list annotations
+            _nodes.push(i);
+            _nodes[*idx].children.push(*idx + 1);
+            _nodes[*idx].idx = *idx;
+            let r1 = vec![*idx];
+            *idx += 1;
+            _nodes.push(e);
+            _nodes[*idx].parents.push(*idx - 1);
+            _nodes[*idx].idx = *idx;
+            let r2 = vec![*idx];
+            *idx += 1;
+            (r1, r2)
+        }
+        Stmt::Noop(pos) => {
+            let mut i = GraphNode::default();
+            i.label = String::from("NoopStart");
+            i.guards.push(Expr::True(pos.clone()));
+            let mut e = GraphNode::default();
+            e.label = String::from("NoopEnd");
+            // XXX: Put these in the _nodes
+            _nodes.push(i);
+            _nodes[*idx].children.push(*idx + 1);
+            _nodes[*idx].idx = *idx;
+            let r1 = vec![*idx];
+            *idx += 1;
+            _nodes.push(e);
+            _nodes[*idx].parents.push(*idx - 1);
+            _nodes[*idx].idx = *idx;
+            let r2 = vec![*idx];
+            *idx += 1;
+            (r1, r2)
+        }
+        Stmt::Block(_stmts, _pos) => rewrite_to_graph_fsm(&_stmts, idx, _nodes),
+	// XXX: With no else branch
+        Stmt::Present(_expr, _tb, None, _pos) => {
+            // XXX: First make the body
+            let (_tr1, _tr2) = rewrite_stmt_to_graph_fsm(_nodes, idx, _tb);
+            let (_er1, _er2) = rewrite_stmt_to_graph_fsm(_nodes, idx, &Stmt::Noop(_pos.clone()));
+
+            // XXX: Now make the initial node for the if-else statement
+            let ii = *idx;
+            let mut i = GraphNode::default();
+            i.idx = ii;
+            i.label = String::from("PresentStart");
+            // XXX: Add the edges between i and nodes in _tr1 and _er1
+            _tr1.into_iter().for_each(|x| {
+                i.children.push(x);
+                _nodes[x].parents.push(ii);
+            });
+            _er1.into_iter().for_each(|x| {
+                i.children.push(x);
+                _nodes[x].parents.push(ii);
+            });
+            // XXX: Push the node to the vector
+            _nodes.push(i);
+            *idx += 1;
+
+            // XXX: Returning the initial and end indices
+            let mut yy = _tr2;
+            let mut yy1 = _er2;
+            yy.append(&mut yy1);
+            (vec![ii], yy)
+        }
+	// XXX: With an else branch
+        Stmt::Present(_expr, _tb, Some(_eb), _pos) => {
+            // XXX: First make the body
+            let (_tr1, _tr2) = rewrite_stmt_to_graph_fsm(_nodes, idx, _tb);
+            let (_er1, _er2) = rewrite_stmt_to_graph_fsm(_nodes, idx, _eb);
+
+            // XXX: Now make the initial node for the if-else statement
+            let ii = *idx;
+            let mut i = GraphNode::default();
+            i.idx = ii;
+            i.label = String::from("PresentStart");
+            // XXX: Add the edges between i and nodes in _tr1 and _er1
+            _tr1.into_iter().for_each(|x| {
+                i.children.push(x);
+                _nodes[x].parents.push(ii);
+            });
+            _er1.into_iter().for_each(|x| {
+                i.children.push(x);
+                _nodes[x].parents.push(ii);
+            });
+            // XXX: Push the node to the vector
+            _nodes.push(i);
+            *idx += 1;
+
+            // XXX: Returning the initial and end indices
+            let mut yy = _tr2;
+            let mut yy1 = _er2;
+            yy.append(&mut yy1);
+            (vec![ii], yy)
+        }
+        Stmt::Abort(_a, _at, _body, _pos) => todo!(),
+        Stmt::Suspend(_a, _at, _body, _pos) => todo!(),
+        Stmt::Loop(_body, _pos) => todo!(),
+        Stmt::Spar(_stmts, _pos) => todo!(),
+    }
+}
+
 #[allow(dead_code)]
-fn rewrite_to_graph_fsm(_v: Vec<Stmt>) -> GraphNode {
-    // TODO: Write this code.
-    GraphNode::default()
+fn rewrite_to_graph_fsm(
+    _v: &[Stmt],
+    _idx: &mut Index,
+    _nodes: &mut Vec<GraphNode>,
+) -> (Vec<Index>, Vec<Index>) {
+    let mut r1 = vec![];
+    let mut r2 = vec![];
+    let mut _pe: Vec<Index> = vec![];
+    for (ii, i) in _v.iter().enumerate() {
+        let (si, ei) = rewrite_stmt_to_graph_fsm(_nodes, _idx, i);
+        if ii == 0 {
+            r1 = si.clone();
+        }
+        if ii == _v.len() - 1 {
+            r2 = ei.clone();
+        }
+        // XXX: Now you need to merge the nodes one after the other
+        if ii > 0 {
+            for k in _pe {
+                for &l in si.iter() {
+                    _nodes[k].children.push(l);
+                    _nodes[l].parents.push(k);
+                }
+            }
+        }
+        _pe = ei;
+    }
+    // TODO: Change this later
+    (r1, r2)
 }
